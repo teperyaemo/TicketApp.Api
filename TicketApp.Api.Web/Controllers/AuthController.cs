cbaa2125abdca380ff2007b69cd8fad1b55c8b37
rokudenas@bk.ru <rokudenas@bk.ru>
2025-05-18 22:59:55 +0300

    fix

--- TicketApp.Api.Web/Controllers/AuthController.cs
+++ TicketApp.Api.Web/Controllers/AuthController.cs
@@ -24,21 +24,33 @@ public class AuthController : ControllerBase
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public async Task<ActionResult> Register([FromBody] RegisterRequest request, CancellationToken cancellationToken)
     {
-        try
+        var allowedChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=[]{};':\",./<>?\\|";
+
+        if (!string.IsNullOrWhiteSpace(request.UserName) && 
+            !string.IsNullOrWhiteSpace(request.Password) &&
+            request.UserName.Length > 5 && 
+            request.Password.Length > 5 && 
+            request.UserName.All(c => allowedChars.Contains(c) && 
+            request.Password.All(x => allowedChars.Contains(x))))
         {
-            var token = await _authService.RegisterAsync(request.UserName, request.Password, cancellationToken);
-            return Ok(new LoginResponse { Token = token });
+            try
+            {
+                var token = await _authService.RegisterAsync(request.UserName, request.Password, cancellationToken);
+                return Ok(new LoginResponse { Token = token });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { Error = ex.Message });
+            }
         }
-        catch (Exception ex)
-        {
-            return BadRequest(new { Error = ex.Message });
-        }
+        
+        return BadRequest(new { Error = "Некорректные логин или пароль" });
     }
 
     [HttpPost("login")]
     [AllowAnonymous]
     [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public async Task<IActionResult> Login([FromBody] LoginRequest request, CancellationToken cancellationToken)
     {
         try
